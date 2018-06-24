04466a7e03754e273f6d55519cbfece7af091ad5
jjayp4rk <jpark0227@gmail.com>
2018-06-24 00:46:36 -0400

    passport and jwt functionality added

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    "mongodb://jayp4rk:1234poop@ds263710.mlab.com:63710/devconnectortest"
+    'mongodb://jayp4rk:1234poop@ds263710.mlab.com:63710/devconnectortest',
+  secretOrKey: 'secret'
 };
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,6 +1,10 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
-router.get("/test", (req, res) => res.json({ msg: "Posts works" }));
+// @route   GET api/posts/test
+// @desc    Tests posts route
+// @access  Public
+
+router.get('/test', (req, res) => res.json({ msg: 'Posts works' }));
 
 module.exports = router;
